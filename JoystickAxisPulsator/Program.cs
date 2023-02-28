0f3d935865af539ac6828dac86b17c8640d13401
SOM-0x3B1 <somadagaszkar@gmail.com>
2023-02-28 14:00:29 +0100

    Add a basic welcome prompt

--- JoystickAxisPulsator/Program.cs
+++ JoystickAxisPulsator/Program.cs
@@ -12,24 +12,53 @@ namespace JoystickAxisPulsator
         static void Main(string[] args)
         {
             // Initialize DirectInput
-            var directInput = new DirectInput();
+            DirectInput directInput = new DirectInput();
+            Guid joystickGuid = Guid.Empty;
 
-            // Find a Joystick Guid
-            var joystickGuid = Guid.Empty;
 
-            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad,
-                        DeviceEnumerationFlags.AllDevices))
-                joystickGuid = deviceInstance.InstanceGuid;
+            #region welcomePrompt
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Welcome to Joystick Axis Pulsator v0.1");
 
-            // If Gamepad not found, look for a Joystick
-            if (joystickGuid == Guid.Empty)
-                foreach (var deviceInstance in directInput.GetDevices(DeviceType.Joystick,
-                        DeviceEnumerationFlags.AllDevices))
-                    joystickGuid = deviceInstance.InstanceGuid;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\nDisclaimer: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("The program will read the values of the selected joystick, \n" +
+                "and output an alternating pulse of keypresses to the selected window. \n" +
+                "These emulated keypresses may cause some unexpected issues with your programs \n" +
+                "and system. By accepting the following propmt, you acknowledge that \n" +
+                "I (the developer of this tool) do not take responsibility for any of these issues. \n" +
+                "Use this software at your own risk.");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\nDo you accept these terms? [Y/N]: ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string input = Console.ReadLine().ToLower();
+            while(input != "n" && input != "y")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nInvalid format. Please use 'Y' or 'N'.");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Do you accept these terms? [Y/N]: ");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                input = Console.ReadLine().ToLower();
+            }
+            if(input == "n")
+                Environment.Exit(0);
+            #endregion
+
+
+            List<DeviceInstance> gamepads = directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices).ToList();
+            List<DeviceInstance> joysticks = directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices).ToList();
 
             // If Joystick not found, throws an error
-            if (joystickGuid == Guid.Empty)
+            if (gamepads.Count + joysticks.Count == 0)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("No joystick/Gamepad found.");
                 Console.ReadKey();
                 Environment.Exit(1);
