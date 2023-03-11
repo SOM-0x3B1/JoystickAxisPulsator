13bf1ff6704c61e27b0a8c6d3219027d93e30997
SOM-0x3B1 <somadagaszkar@gmail.com>
2023-03-11 15:48:22 +0100

    Update assemly information

--- JoystickAxisPulsator/Properties/AssemblyInfo.cs
+++ JoystickAxisPulsator/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,10 +7,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JoystickAxisPulsator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A makeshift app for Windows that makes joysticks compatible with games without axis support.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JoystickAxisPulsator")]
+[assembly: AssemblyCompany("Onekilobit Studios")]
+[assembly: AssemblyProduct("Joystick Axis Pulsator")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
